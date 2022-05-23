232e8c9a19bded3e1bda835d6cd9370fc37ecf04
Mark Kvetny <m.kvetny@outlook.com>
2022-05-23 21:33:12 +0200

    initial commit

---
+++ src/components/Test.tsx
@@ -0,0 +1,10 @@
+// import { Motion } from "@motionone/solid";
+
+export const Test = () => {
+  return (
+    <div>
+      <h1>Test</h1>
+      {/* <Motion.p>this is a test</Motion.p> */}
+    </div>
+  );
+};
